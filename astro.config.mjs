45cc5b81577420e4cd7735307f6a4d21d4a29ba4
Dmitry Kotik <7944694+dkotik@users.noreply.github.com>
2023-12-12 09:58:33 +0100

    Remove astro-compress run npm fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
-import compress from "astro-compress";
+// import compress from "astro-compress";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
 import astroI18next from "astro-i18next";
@@ -32,15 +32,15 @@ export default defineConfig({
   integrations: [
     svelte(),
     astroI18next(),
-    compress({
-      // path: target,
-      // logger: 0, // default 2
-      // css: false,
-      // html: false,
-      // js: false,
-      // img: false,
-      // svg: false
-    }),
+    // compress({
+    //   // path: target,
+    //   // logger: 0, // default 2
+    //   // css: false,
+    //   // html: false,
+    //   // js: false,
+    //   // img: false,
+    //   // svg: false
+    // }),
     robotsTxt({
       sitemapBaseFileName: "sitemap-index",
       // default 'sitemap-index'
